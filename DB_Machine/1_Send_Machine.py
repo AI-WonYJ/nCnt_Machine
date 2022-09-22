ddddf396732fc3aea37bcaffdbe89f6a58ae0a70
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-09-22 20:11:43 +0900

    Update and rename Send_Machine.py to 1_Send_Machine.py

--- DB_Machine/1_Send_Machine.py
+++ DB_Machine/1_Send_Machine.py
@@ -2,6 +2,10 @@ import cv2
 from datetime import datetime
 import time
 
+
+old_time = 0
+
+
 def picture():
   cap = cv2.VideoCapture(0)
   if cap.isOpened():
@@ -20,12 +24,19 @@ def picture():
   cap.release()
   cv2.destroyAllWindows()
 
+
+
 def n_time():
+  global old_time
   dt = str(datetime.now())
   current_time = int(dt[17:19])
-  if  current_time % 30 == 0:
-    print(dt[0:19]) #picture()
-    time.sleep(1)
+  if old_time != current_time:
+    old_time = current_time
+    if  current_time % 3 == 0:
+      print(dt[0:19]) #picture()
+
+
+# 변수에 이전 시간 저장하고, 같으면 pass
 
 while True:
   n_time()
