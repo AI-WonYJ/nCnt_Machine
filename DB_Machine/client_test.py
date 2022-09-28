f2e74eb2b827697d1221e5e0bddf4d8c564ce7e5
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-09-28 21:34:16 +0900

    Update client_test.py

--- DB_Machine/client_test.py
+++ DB_Machine/client_test.py
@@ -1,4 +1,5 @@
 import requests
-files = open("DB_Machine\sample.jpg", "rb")
-upload = {'file': files}
-res = requests.post('http://127.0.0.1:5000/image/', files = upload)
+
+files = open("DB_Machine\sample.jpg", "rb")  # 파일을 'rb'방식으로 연다.
+upload = {'file': files}  # 딕셔너리 형식으로 파일 설정
+res = requests.post('http://127.0.0.1:5000/', files = upload)  # requests.post(url, data = None, json = None, **kwargs)
