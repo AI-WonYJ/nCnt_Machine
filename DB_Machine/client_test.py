7e9129fffba8145f99a93781927096a62f9d446a
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-11-01 23:56:52 +0900

    Delete client_test.py

--- DB_Machine/client_test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-files = open("DB_Machine\sample.jpg", "rb")  # 파일을 'rb'방식으로 연다.
-upload = {'file': files}  # 딕셔너리 형식으로 파일 설정
-res = requests.post('http://127.0.0.1:5000/', files = upload)  # requests.post(url, data = None, json = None, **kwargs)
